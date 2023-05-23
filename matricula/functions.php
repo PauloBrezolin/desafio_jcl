148cda27e32bf28d52651fc7da1e9d4d2cc00c7f
Paulo Brezolin de Oliveira <112288627+PauloBrezolin@users.noreply.github.com>
2023-05-22 21:07:30 -0300

    Update functions.php

--- matricula/functions.php
+++ matricula/functions.php
@@ -1,21 +1,17 @@
 <?php
 function validarCadastro($nome, $email, $curso_id, $db) {
-    // Verifica se o nome foi informado
     if (empty($nome)) {
         return "O campo Nome é obrigatório.";
     }
 
-    // Verifica se o email foi informado
     if (empty($email)) {
         return "O campo E-mail é obrigatório.";
     }
 
-    // Verifica se um curso foi escolhido
     if (empty($curso_id)) {
         return "É necessário escolher um curso.";
     }
 
-    // Verifica se o aluno já foi cadastrado anteriormente
     $query = $db->prepare("SELECT COUNT(*) FROM alunos WHERE email = ?");
     $query->execute([$email]);
     $count = $query->fetchColumn();
@@ -24,6 +20,6 @@ function validarCadastro($nome, $email, $curso_id, $db) {
         return "O aluno já foi cadastrado anteriormente.";
     }
 
-    return ""; // Retornar uma string vazia indica que não houve erros
+    return "";
 }
 ?>
