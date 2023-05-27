8f2df283f8d9bb0edeef116c9e88acfa40a6c5fc
Paulo Brezolin de Oliveira <112288627+PauloBrezolin@users.noreply.github.com>
2023-05-27 11:47:39 -0300

    Update excluir.php

--- manipular_informações/excluir.php
+++ manipular_informações/excluir.php
@@ -5,8 +5,6 @@ if (isset($_POST['id'])) {
     $id = $_POST['id'];
 
     try {
-        $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $stmt = $db->prepare("DELETE FROM alunos WHERE id = :id");
         $stmt->bindParam(':id', $id);
