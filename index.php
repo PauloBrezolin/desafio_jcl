4f99025ace3113235e2752684aeac664b72479e5
Paulo Brezolin de Oliveira <112288627+PauloBrezolin@users.noreply.github.com>
2023-05-29 10:22:33 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+</head>
+<body>
+    <h1>Escolha como deseja entrar: </h1>
+    <button onclick="window.location.href = 'matricula/index.php';">ALUNO</button>
+    <button onclick="window.location.href = 'manipular_informações/index.php';">PROFESSOR</button>
+</body>
+</html>
